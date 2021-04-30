2b5b0537e1ab9d014adf31bf80f6b0ec0bd27b10
Stefan9241 <Stefan9241@abv.bg>
2021-05-01 00:37:12 +0300

    Added first and second problem

---
+++ Fundamentals/MidExam05/01.BlackFlag/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01.BlackFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double daysPirating = double.Parse(Console.ReadLine());
+            double dailyPlunder = double.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPLunder = 0;
+            for (int i = 1; i <= daysPirating; i++)
+            {
+                totalPLunder += dailyPlunder;
+                if (i % 3 == 0)
+                {
+                    double additionalPlunder = dailyPlunder / 2;
+                    totalPLunder += additionalPlunder;
+                }
+                if (i % 5 == 0)
+                {
+                    double minusPlunder = totalPLunder * 0.30;
+                    totalPLunder -= minusPlunder;
+                }
+                
+            }
+            if (totalPLunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPLunder:f2} plunder gained.");
+            }
+            else
+            {
+                double percentagePlunder = totalPLunder / expectedPlunder * 100;
+                Console.WriteLine($"Collected only {percentagePlunder:f2}% of the plunder.");
+            }
+        }
+    }
+}
---
+++ Fundamentals/MidExam05/02.TreasureHunt/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasure = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> stolenItems = new List<string>();
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (commands[0] != "Yohoho!")
+            {
+                if (commands[0] == "Loot")
+                {
+
+
+                    for (int i = 1; i < commands.Length; i++)
+                    {
+                        if (!treasure.Contains(commands[i]))
+                        {
+                            treasure.Insert(0, commands[i]);
+                        }
+                    }
+
+
+                }
+                else if (commands[0] == "Drop")
+                {
+
+                    int index = int.Parse(commands[1]);
+                    if (index > treasure.Count - 1 || index < 0)
+                    {
+                        commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        continue;
+                    }
+                    string item = treasure[index];
+                    treasure.RemoveAt(index);
+                    treasure.Add(item);
+                }
+                else if (commands[0] == "Steal")
+                {
+                    int count = int.Parse(commands[1]);
+
+                    if (count > treasure.Count - 1)
+                    {
+                        Console.WriteLine(string.Join(", ", treasure));
+                        treasure.RemoveRange(0, treasure.Count);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int indexToRemove = treasure.Count - 1;
+                            string stolenItem = treasure[indexToRemove];
+                            treasure.RemoveAt(indexToRemove);
+                            stolenItems.Add(stolenItem);
+                        }
+                        stolenItems.Reverse();
+                        Console.WriteLine(string.Join(", ", stolenItems));
+                        stolenItems.Clear();
+                    }
+                }
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            double totalSum = 0;
+            for (int i = 0; i < treasure.Count; i++)
+            {
+                double sum = treasure[i].Count();
+                totalSum += sum;
+            }
+            double countTreasures = treasure.Count;
+            totalSum = totalSum / countTreasures;
+
+            if (treasure.Count <= 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+            else
+            {
+                Console.WriteLine($"Average treasure gain: {totalSum:f2} pirate credits.");
+            }
+        }
+    }
+}
