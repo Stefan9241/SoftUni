e4bcaea82b71b45017848dc18cd95f0bbc2e3c3c
Stefan9241 <Stefan9241@abv.bg>
2021-05-12 18:00:45 +0300

    Added 2 more exercise for TextProcessingExercise

---
+++ Fundamentals/TextProcessingExercise/06.StringExplosion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int bomb = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char curChar = input[i];
+
+                if (curChar == '>')
+                {
+                    int power = int.Parse(input[i + 1].ToString());
+                    bomb += power;
+                    continue;
+                }
+                if (bomb > 0)
+                {
+
+                    input = input.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Fundamentals/TextProcessingExercise/08.Alphabet/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08.Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            string alpha = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            double totalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string curNumber = input[i];
+                char firstLetter = curNumber[0];
+                char lastLetter = curNumber[curNumber.Length - 1];
+                double number = double.Parse(curNumber.Substring(1, curNumber.Length - 2));
+                double currSum = 0;
+                
+                if (alpha.Contains(firstLetter))
+                {
+                    int firstLetterPosition = alpha.IndexOf(firstLetter) + 1;
+                    currSum = number / firstLetterPosition;
+                    
+                }
+                else
+                {
+                    string lowerLetter = firstLetter.ToString().ToUpper();
+                    int firstLetterPosition = alpha.IndexOf(lowerLetter) + 1;
+                    currSum = number * firstLetterPosition;
+                    
+                }
+
+                if (alpha.Contains(lastLetter))
+                {
+                    int lastLetterPosition = alpha.IndexOf(lastLetter) + 1;
+                    currSum -= lastLetterPosition;
+                }
+                else
+                {
+                    string lowerLetter = lastLetter.ToString().ToUpper();
+                    int lastLetterPositions = alpha.IndexOf(lowerLetter) + 1;
+                    currSum += lastLetterPositions;
+                }
+
+                totalSum += currSum;
+            }
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
