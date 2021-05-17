40eb8bd3afda7f0073b91a4f57f4b9ded8707cb3
Stefan9241 <Stefan9241@abv.bg>
2021-05-17 14:00:46 +0300

    Added RegularExpresions Lab

---
+++ Fundamentals/RegularExpresions/01.MatchFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match name in matches)
+            {
+                Console.Write(name.Value + " ");
+            }
+        }
+    }
+}
---
+++ Fundamentals/RegularExpresions/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+            string phones = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(phones, pattern);
+
+            string[] matchPhones = phoneMatches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchPhones));
+
+        }
+    }
+}
