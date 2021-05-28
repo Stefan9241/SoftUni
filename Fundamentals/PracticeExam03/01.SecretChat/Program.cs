ce103267a05e7c7d4551d7b074d9f0d17702f53a
Stefan9241 <Stefan9241@abv.bg>
2021-05-29 00:30:16 +0300

    Added PracticalExam03 string and RegEx problems

---
+++ Fundamentals/PracticeExam03/01.SecretChat/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+namespace _01.SecretChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMsg = Console.ReadLine();
+
+            string[] commands = Console.ReadLine().Split(":|:",StringSplitOptions.RemoveEmptyEntries);
+            while (commands[0] != "Reveal")
+            {
+                if (commands[0] == "InsertSpace")
+                {
+                    int index = int.Parse(commands[1]);
+                    secretMsg = secretMsg.Insert(index, " ");
+                    Console.WriteLine(secretMsg);
+                }
+                else if (commands[0] == "Reverse")
+                {
+                    string substring = commands[1];
+                    char[] charArray = substring.ToCharArray();
+                    Array.Reverse(charArray);
+                    string reversedSubstring = string.Join("", charArray);
+                    int indexOf = secretMsg.IndexOf(substring);
+                    int lenght = substring.Length;
+                    if (secretMsg.Contains(substring))
+                    {
+                        secretMsg = secretMsg.Remove(indexOf,lenght);
+                        secretMsg = secretMsg.Insert(secretMsg.Length, reversedSubstring);
+                        Console.WriteLine(secretMsg);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (commands[0] == "ChangeAll")
+                {
+                    string substring = commands[1];
+                    string replacement = commands[2];
+                    secretMsg = secretMsg.Replace(substring, replacement);
+                    Console.WriteLine(secretMsg);
+                }
+
+                commands = Console.ReadLine().Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"You have a new text message: {secretMsg}");
+        }
+    }
+}
---
+++ Fundamentals/PracticeExam03/02.MirrorWords/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string wordValidatePattern = @"\#{1}[A-z]{3,}\#{2}[A-z]{3,}\#{1}|\@{1}[A-z]{3,}\@{2}[A-z]{3,}\@{1}";
+            Regex regex = new Regex(wordValidatePattern);
+            MatchCollection matches = regex.Matches(text);
+            List<string> validPairs = new List<string>();
+            List<string> mirroredPairs = new List<string>();
+
+            foreach (Match item in matches)
+            {
+                validPairs.Add(item.ToString());
+            }
+
+            for (int i = 0; i < validPairs.Count; i++)
+            {
+                string currentPair = validPairs[i];
+                string firstWord = "";
+                string secondWord = "";
+
+                for (int j = 0; j < currentPair.Length / 2; j++)
+                {
+                    firstWord += currentPair[j];
+                }
+
+                for (int k = currentPair.Length - 1; k >= currentPair.Length / 2; k--)
+                {
+                    secondWord += currentPair[k];
+                }
+
+                if (firstWord == secondWord)
+                {
+                    mirroredPairs.Add(currentPair);
+                }
+                
+            }
+
+            if (mirroredPairs.Count == 0)
+            {
+                if (validPairs.Count != 0)
+                {
+                    Console.WriteLine($"{validPairs.Count} word pairs found!");
+                    Console.WriteLine("No mirror words!");
+                }
+                else
+                {
+                    Console.WriteLine("No word pairs found!");
+                    Console.WriteLine("No mirror words!");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine($"{validPairs.Count} word pairs found!");
+                Console.WriteLine("The mirror words are:");
+            }
+
+            List<string> finalResult = new List<string>();
+
+            for (int i = 0; i < mirroredPairs.Count; i++)
+            {
+                string current = mirroredPairs[i];
+                int endIndex = (current.Length / 2) - 2;
+                string key = current.Substring(1, endIndex);
+                string value = current.Substring(current.Length / 2 + 1,current.Length - (current.Length / 2) - 2);
+
+                finalResult.Add($"{key} <=> {value}");
+            }
+
+            Console.WriteLine(string.Join(", ", finalResult));
+        }
+    }
+}
