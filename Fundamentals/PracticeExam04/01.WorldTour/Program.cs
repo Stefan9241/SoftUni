5160031d147e1a845bffbc7ebf9236c3a175ed42
Stefan9241 <Stefan9241@abv.bg>
2021-05-30 19:55:32 +0300

    Added PracticalExam04

---
+++ Fundamentals/PracticeExam04/01.WorldTour/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _01.WorldTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] cmdArgs = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmdArgs[0] != "Travel")
+            {
+                if (cmdArgs[0] == "Add Stop")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string newString = cmdArgs[2];
+                    if (index >= 0 && index <= text.Length - 1)
+                    {
+                        text = text.Insert(index, newString);
+                    }
+                }
+                else if (cmdArgs[0] == "Remove Stop")
+                {
+                    int startIndex = int.Parse(cmdArgs[1]);
+                    int endIndex = int.Parse(cmdArgs[2]);
+                    string stringToDelete = "";
+                    if (startIndex >= 0 && startIndex <= text.Length - 1 && endIndex >= 0 && endIndex <= text.Length - 1)
+                    {
+                        for (int i = startIndex; i <= endIndex; i++)
+                        {
+                            stringToDelete += text[i];
+                        }
+                        text = text.Remove(startIndex, stringToDelete.Length);
+                    }
+                }
+                else if (cmdArgs[0] == "Switch")
+                {
+                    string oldString = cmdArgs[1];
+                    string newString = cmdArgs[2];
+                    if (text.Contains(oldString))
+                    {
+                        text = text.Replace(oldString, newString);
+                    }
+                }
+                Console.WriteLine(text);
+                cmdArgs = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {text}");
+        }
+    }
+}
---
+++ Fundamentals/PracticeExam04/02.DestinationMapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(=|\/)[A-Z][A-Za-z]{2,}\1"; ;
+            Regex validator = new Regex(pattern);
+            MatchCollection match = validator.Matches(input);
+            int travelPoints = 0;
+            List<string> destinations = new List<string>();
+            for (int i = 0; i < match.Count; i++)
+            {
+                string currentMatch = match[i].ToString();
+                travelPoints += currentMatch.Length - 2;
+                string destination = currentMatch.Substring(1, (currentMatch.Length - 1) - 1);
+                destinations.Add(destination);
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
