db3631d34662c616f63bd5fe55d74215070e7eaf
Stefan9241 <Stefan9241@abv.bg>
2021-06-09 18:48:35 +0300

    Added 2 more MultidimensionalArraysLab Problems

---
+++ C#-Advanced/MultidimensionalArrays/06.JaggedArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRows = int.Parse(Console.ReadLine());
+            int[][] jaggedMatrix = new int[matrixRows][];
+
+            for (int row = 0; row < matrixRows; row++)
+            {
+                jaggedMatrix[row] = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                
+            }
+
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (commands[0].ToUpper() != "END")
+            {
+                int row = int.Parse(commands[1]);
+                int col = int.Parse(commands[2]);
+                int value = int.Parse(commands[3]);
+                if (row < 0 || row >= jaggedMatrix.Length || col < 0 || col >= jaggedMatrix[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    continue;
+                }
+
+                switch (commands[0])
+                {
+                    case "Add":
+                        jaggedMatrix[row][col] += value;
+                        break;
+                    case "Subtract":
+                        jaggedMatrix[row][col] -= value;
+                        break;
+                    
+                }
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in jaggedMatrix)
+            {
+                Console.WriteLine(string.Join(" ",item));
+            }
+        }
+    }
+}
---
+++ C#-Advanced/MultidimensionalArrays/07.PascalTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[num][];
+
+            for (int i = 0; i < num; i++)
+            {
+                long[] row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    row[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+                }
+
+                triangle[i] = row;
+            }
+
+            foreach (var item in triangle)
+            {
+                Console.WriteLine(string.Join(" ",item));
+            }
+        }
+    }
+}
