4a0a79f48f0f317cf526de9579694bf50e87ca93
Stefan9241 <Stefan9241@abv.bg>
2021-06-11 12:54:40 +0300

    Added MultidimentionalArraysExercise 1, 2 and 3 problem

---
+++ C#-Advanced/MultidimentionalArraysExercise/01.DiagonalDiff/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int primeSum = 0;
+            int secSum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                primeSum += matrix[i, i];
+                secSum += matrix[i, n - i - 1];
+            }
+
+            Console.WriteLine($"{Math.Abs(primeSum - secSum)}");
+        }
+    }
+}
---
+++ C#-Advanced/MultidimentionalArraysExercise/02.SqrsInMatrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SqrsInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNums = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[inputNums[0], inputNums[1]];
+            int count = 0;
+            for (int row = 0; row < inputNums[0]; row++)
+            {
+                char[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < inputNums[1]; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row,col] == matrix[row+1,col]
+                        && matrix[row,col] == matrix[row,col + 1]
+                        && matrix[row,col] == matrix[row+1,col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
