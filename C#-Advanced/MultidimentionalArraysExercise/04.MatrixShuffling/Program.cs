1bd712bb908ac55d9d645de4d4f4576192e45beb
Stefan9241 <Stefan9241@abv.bg>
2021-06-12 20:40:22 +0300

    Added 2 more problems in Matrix

---
+++ C#-Advanced/MultidimentionalArraysExercise/04.MatrixShuffling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputDimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+            int rows = inputDimensions[0];
+            int cols = inputDimensions[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+
+                string[] tokensNums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = tokensNums[col];
+                }
+            }
+
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (commands[0] != "END")
+            {
+                int row1 = int.Parse(commands[1]);
+                int col1 = int.Parse(commands[2]);
+
+                
+                if (commands[0] == "swap" && row1 < rows && col1 < cols && commands.Length == 5)
+                {
+                    string temp = matrix[row1, col1];
+                    int row2 = int.Parse(commands[3]);
+                    int col2 = int.Parse(commands[4]);
+                    matrix[row1, col1] = matrix[row2, col2];
+                    matrix[row2, col2] = temp;
+
+                    for (int row = 0; row < rows; row++)
+                    {
+                        for (int col = 0; col < cols; col++)
+                        {
+                            Console.Write(matrix[row,col] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
