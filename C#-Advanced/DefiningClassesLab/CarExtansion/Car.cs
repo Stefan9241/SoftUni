806be270188e90ffa4c8bd2bba7a18be3ad9ad52
Stefan9241 <Stefan9241@abv.bg>
2021-06-28 19:54:34 +0300

    Added Defining ClassesLab 1-5

---
+++ C#-Advanced/DefiningClassesLab/CarExtansion/Car.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQantity;
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            set { fuelConsumption = value; }
+            get { return fuelConsumption; }
+        }
+        public double FuelQuantity
+        {
+            set { fuelQantity = value; }
+            get { return fuelQantity; }
+        }
+        public string Make
+        {
+            set
+            { make = value; }
+            get { return make; }
+        }
+
+        public string Model
+        {
+            set
+            {
+                model = value;
+            }
+            get
+            {
+                return model;
+            }
+        }
+
+        public int Year
+        {
+            set
+            {
+                year = value;
+            }
+            get
+            {
+                return year;
+            }
+        }
+        public void Drive(double distance)
+        {
+            bool canContinue = this.fuelQantity - (distance * this.fuelConsumption) >= 0;
+            if (canContinue)
+            {
+                this.fuelQantity -= distance * this.fuelConsumption;
+            }
+            else
+            {
+                System.Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.make}");
+            sb.AppendLine($"Model: {this.model}");
+            sb.AppendLine($"Year: {this.year}");
+            sb.Append($"Fuel: {this.fuelQantity:f2}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClassesLab/CarManufacturer/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+      
+        public string Make
+        {
+            set
+            { make = value; }
+            get { return make; }
+        }
+
+        public string Model
+        {
+            set
+            {
+                model = value;
+            }
+            get
+            {
+                return model;
+            }
+        }
+
+        public int Year
+        {
+            set
+            {
+                year = value;
+            }
+            get
+            {
+                return year;
+            }
+        }
+    }
+}
