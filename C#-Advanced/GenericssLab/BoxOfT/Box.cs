e3a4c9ea1685410bbee93b572a84520d05a7f7a4
Stefan9241 <Stefan9241@abv.bg>
2021-07-09 23:25:14 +0300

    Added GenericsLab

---
+++ C#-Advanced/GenericssLab/BoxOfT/Box.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.items.Count;
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public T Remove()
+        {
+            var item = this.items[items.Count - 1];
+            this.items.RemoveAt(items.Count - 1);
+
+            return item;
+        }
+    }
+}
