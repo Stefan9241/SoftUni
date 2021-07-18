1cb4d94540a0bc336d7b8beaba58fccce6d0d64f
Stefan9241 <Stefan9241@abv.bg>
2021-07-18 15:55:06 +0300

    Added IteratorsAndComparatorsExercise

---
+++ C#-Advanced/IteratorsAndComparatorsExercise/ComparingObjects/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private string town;
+        private int age;
+
+        public Person(string name,int age,string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+        public string Name => name;
+        public string Town => town;
+        public int Age => age;
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+
+            if (result == 0)
+            {
+                result = Town.CompareTo(other.Town);
+            }
+            return result;
+        }
+    }
+}
---
+++ C#-Advanced/IteratorsAndComparatorsExercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+        public List<T> List => collection;
+        public bool Move()
+        {
+            bool canMove = this.HasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidProgramException("Invalid operation");
+            }
+            Console.WriteLine($"{collection[currIndex]}");
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine(string.Join(" ",collection));
+        }
+        public bool HasNext() => currIndex < collection.Count - 1;
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T element in collection)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
