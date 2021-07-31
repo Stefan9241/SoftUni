98b664f94c91029a87b6f403790feef0a062461f
Stefan9241 <Stefan9241@abv.bg>
2021-07-31 14:11:02 +0300

    Added Restaurant in InheritanceExercise

---
+++ C# OOP/InheritanceExercise/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultGrams = 250;
+        private const double DefaultCals = 1000;
+        private const decimal DefaultPrice = 5m;
+        public Cake(string name) 
+            : base(name, DefaultPrice, DefaultGrams, DefaultCals)
+        {
+        }
+    }
+}
