3d8995fb58fb705b9edc381348e9f5587481e19e
Stefan9241 <Stefan9241@abv.bg>
2021-08-01 22:23:42 +0300

    EncapsulationExercise 1 problem

---
+++ C# OOP/EncapsulationExercise/ClassBoxData/Box.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double len, double wid, double heig)
+        {
+            Length = len;
+            Width = wid;
+            Height = heig;
+        }
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return (2 * length * width) + (2 * length * height) + (2 * width * height);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return (2 * length * height) + (2 * width * height);
+        }
+
+        public double Volume()
+        {
+            return length * width * height;
+        }
+    }
+}
---
+++ C# OOP/EncapsulationExercise/ClassBoxData/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(lenght, width, height);
+                double surface = box.SurfaceArea();
+                Console.WriteLine($"Surface Area - {surface:f2}");
+                double lateral = box.LateralSurfaceArea();
+                Console.WriteLine($"Lateral Surface Area - {lateral:f2}");
+                double volume = box.Volume();
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
