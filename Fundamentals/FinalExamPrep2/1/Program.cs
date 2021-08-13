e44e53f0f92141ee29997fb2d474bcfaebeba1a5
Stefan9241 <Stefan9241@abv.bg>
2021-08-13 23:48:16 +0300

    FinalExamPrep2 -1

---
+++ Fundamentals/FinalExamPrep2/1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            var tokens = Console.ReadLine().Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+            while (tokens[0] != "Reveal")
+            {
+                if (tokens[0] == "InsertSpace")
+                {
+                    int index = int.Parse(tokens[1]);
+                    message = message.Insert(index, " ");
+                    Console.WriteLine(message);
+                }
+                else if(tokens[0] == "Reverse")
+                {
+                    string substring = tokens[1];
+                    if (message.Contains(substring))
+                    {
+                        int indexOfSubstring = message.IndexOf(substring);
+                        message = message.Remove(indexOfSubstring,substring.Length);
+                        var sb = new StringBuilder();
+                        for (int i = substring.Length - 1; i >= 0; i--)
+                        {
+                            sb.Append(substring[i]);
+                        }
+                        message = message.Insert(message.Length, sb.ToString());
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if(tokens[0] == "ChangeAll")
+                {
+                    string substringToReplace = tokens[1];
+                    string replacement = tokens[2];
+                    message = message.Replace(substringToReplace, replacement);
+                    Console.WriteLine(message);
+                }
+
+
+                tokens = Console.ReadLine().Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
