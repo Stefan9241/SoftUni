1884cc9641f8671fa411a5d57fc997854bce0ed6
stefan spirov <ssspirov@abv.bg>
2021-08-19 18:24:35 +0300

    Added VehiclesFirst

---
+++ C# OOP/PolymorphismExercise/VehiclesFirst/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesFirst
+{
+    public class Car : Vehicle
+    {
+        private const double AirCond = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption)
+        {
+
+        }
+
+        public override void Refueling(double liters)
+        {
+            this.FuelQuantity += liters;
+        }
+
+        public override double AirCondition()
+        {
+            return AirCond;
+        }
+    }
+}
---
+++ C# OOP/PolymorphismExercise/VehiclesFirst/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace VehiclesFirst
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var line = LineRead();
+            var car = new Car(double.Parse(line[1]), double.Parse(line[2]));
+            line = LineRead();
+            var truck = new Truck(double.Parse(line[1]), double.Parse(line[2]));
+            int nLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nLines; i++)
+            {
+                line = LineRead();
+                // line[0]= cmd
+                // line [1] = type 
+                // line [2] = value;
+
+                var value = double.Parse(line[2]);
+                switch (line[0])
+                {
+                    case "Drive":
+                        if (line[1] is "Car")
+                        {
+                            car.Driving(value);
+                        }
+                        else if (line[1] is "Truck")
+                        {
+                            truck.Driving(value);
+                        }
+                        break;
+                    case "Refuel":
+                        if (line[1] is "Car")
+                        {
+                            car.Refueling(value);
+                        }
+                        else if (line[1] is "Truck")
+                        {
+                            truck.Refueling(value);
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:f2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:f2}");
+
+        }
+
+        private static string[] LineRead()
+        {
+            return Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+        }
+    }
+}
+
