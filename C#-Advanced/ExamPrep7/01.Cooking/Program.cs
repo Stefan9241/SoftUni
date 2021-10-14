f7d3c59329154237db34e4085807694cd7ca0bd9
stefan spirov <ssspirov@abv.bg>
2021-10-14 18:39:03 +0300

    ExamPrep7

---
+++ C#-Advanced/ExamPrep7/01.Cooking/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var stackNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var queueLiquids = new Queue<int>(queueNumbers);
+            var stackIngredientValues = new Stack<int>(stackNumbers);
+
+            int breadCount = 0;
+            int cakeCount = 0;
+            int pastryCount = 0;
+            int fruitPieCount = 0;
+            bool isBread = false;
+            bool isCake = false;
+            bool isPastry = false;
+            bool isFruitPie = false;
+
+            while (queueLiquids.Count > 0 && stackIngredientValues.Count > 0)
+            {
+                var sum = stackIngredientValues.Peek() + queueLiquids.Peek();
+                if (sum == 25)
+                {
+                    breadCount++;
+                    stackIngredientValues.Pop();
+                    queueLiquids.Dequeue();
+                    isBread = true;
+                }
+                else if(sum == 50)
+                {
+                    cakeCount++;
+                    stackIngredientValues.Pop();
+                    queueLiquids.Dequeue();
+                    isCake = true;
+                }
+                else if (sum == 75)
+                {
+                    pastryCount++;
+                    stackIngredientValues.Pop();
+                    queueLiquids.Dequeue();
+                    isPastry = true;
+                }
+                else if (sum == 100)
+                {
+                    fruitPieCount++;
+                    stackIngredientValues.Pop();
+                    queueLiquids.Dequeue();
+                    isFruitPie = true;
+                }
+                else
+                {
+                    queueLiquids.Dequeue();
+                    stackIngredientValues.Push(stackIngredientValues.Pop() + 3);
+                }
+            }
+
+            if (isBread && isCake && isPastry && isFruitPie)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (queueLiquids.Count > 0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ",queueLiquids)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (stackIngredientValues.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", stackIngredientValues)}");
+                
+            }
+            else
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            Console.WriteLine($"Bread: {breadCount}");
+            Console.WriteLine($"Cake: {cakeCount}");
+            Console.WriteLine($"Fruit Pie: {fruitPieCount}");
+            Console.WriteLine($"Pastry: {pastryCount}");
+        }
+    }
+}
---
+++ C#-Advanced/ExamPrep7/02.Selling/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+
+namespace _02.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var matrix = new char[n, n];
+            var heroRow = 0;
+            var heroCol = 0;
+
+            var firstPillarRow = -10;
+            var firstPillarCol = -10;
+
+            bool pillarExist = false;
+
+            var secondPillarRow = -10;
+            var secondPillarColl = -10;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    matrix[i, j] = input[j];
+                    if (matrix[i, j] == 'S')
+                    {
+                        heroRow = i;
+                        heroCol = j;
+                    }
+                    else if (matrix[i, j] == 'O' && pillarExist == false)
+                    {
+                        pillarExist = true;
+                        firstPillarRow = i;
+                        firstPillarCol = j;
+                    }
+                    else if (matrix[i, j] == 'O' && pillarExist == true)
+                    {
+                        secondPillarRow = i;
+                        secondPillarColl = j;
+                    }
+                }
+            }
+
+            int sum = 0;
+            bool isOutOfBounds = false;
+            while (sum < 50)
+            {
+                string command = Console.ReadLine();
+                if (command == "up")
+                {
+                    if (heroRow - 1 < 0)
+                    {
+                        isOutOfBounds = true;
+                        matrix[heroRow, heroCol] = '-';
+                        break;
+                    }
+
+                    if (matrix[heroRow - 1, heroCol] == 'O')
+                    {
+                        if (matrix[heroRow - 1, heroCol] == matrix[firstPillarRow, firstPillarCol])
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow - 1, heroCol] = '-';
+                            matrix[secondPillarRow, secondPillarColl] = 'S';
+                            heroRow = secondPillarRow;
+                            heroCol = secondPillarColl;
+                        }
+                        else
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow - 1, heroCol] = '-';
+                            matrix[firstPillarRow, firstPillarCol] = 'S';
+                            heroRow = firstPillarRow;
+                            heroCol = firstPillarCol;
+                        }
+                    }
+                    else if (char.IsDigit(matrix[heroRow - 1, heroCol]))
+                    {
+                        sum += int.Parse(matrix[heroRow - 1, heroCol].ToString());
+                        matrix[heroRow - 1, heroCol] = 'S';
+                        matrix[heroRow, heroCol] = '-';
+                        heroRow--;
+                    }
+                    else
+                    {
+                        matrix[heroRow, heroCol] = '-';
+                        matrix[heroRow - 1, heroCol] = 'S';
+                        heroRow--;
+                    }
+
+                }
+                else if (command == "down")
+                {
+                    if (heroRow + 1 == n)
+                    {
+                        isOutOfBounds = true;
+                        matrix[heroRow, heroCol] = '-';
+                        break;
+                    }
+
+                    if (matrix[heroRow + 1, heroCol] == 'O')
+                    {
+                        if (matrix[heroRow + 1, heroCol] == matrix[firstPillarRow, firstPillarCol])
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow + 1, heroCol] = '-';
+                            matrix[secondPillarRow, secondPillarColl] = 'S';
+                            heroRow = secondPillarRow;
+                            heroCol = secondPillarColl;
+                        }
+                        else
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow + 1, heroCol] = '-';
+                            matrix[firstPillarRow, firstPillarCol] = 'S';
+                            heroRow = firstPillarRow;
+                            heroCol = firstPillarCol;
+                        }
+                    }
+                    else if (char.IsDigit(matrix[heroRow + 1, heroCol]))
+                    {
+                        sum += int.Parse(matrix[heroRow + 1, heroCol].ToString());
+                        matrix[heroRow + 1, heroCol] = 'S';
+                        matrix[heroRow, heroCol] = '-';
+                        heroRow++;
+                    }
+                    else
+                    {
+                        matrix[heroRow, heroCol] = '-';
+                        matrix[heroRow + 1, heroCol] = 'S';
+                        heroRow++;
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (heroCol - 1 < 0)
+                    {
+                        isOutOfBounds = true;
+                        matrix[heroRow, heroCol] = '-';
+                        break;
+                    }
+
+                    if (matrix[heroRow, heroCol - 1] == 'O')
+                    {
+                        if (matrix[heroRow, heroCol - 1] == matrix[firstPillarRow, firstPillarCol])
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow, heroCol - 1] = '-';
+                            matrix[secondPillarRow, secondPillarColl] = 'S';
+                            heroRow = secondPillarRow;
+                            heroCol = secondPillarColl;
+                        }
+                        else
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow, heroCol - 1] = '-';
+                            matrix[firstPillarRow, firstPillarCol] = 'S';
+                            heroRow = firstPillarRow;
+                            heroCol = firstPillarCol;
+                        }
+                    }
+                    else if (char.IsDigit(matrix[heroRow, heroCol - 1]))
+                    {
+                        sum += int.Parse(matrix[heroRow, heroCol - 1].ToString());
+                        matrix[heroRow, heroCol - 1] = 'S';
+                        matrix[heroRow, heroCol] = '-';
+                        heroCol--;
+                    }
+                    else
+                    {
+                        matrix[heroRow, heroCol] = '-';
+                        matrix[heroRow, heroCol - 1] = 'S';
+                        heroCol--;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (heroCol + 1 == n)
+                    {
+                        isOutOfBounds = true;
+                        matrix[heroRow, heroCol] = '-';
+                        break;
+                    }
+
+                    if (matrix[heroRow, heroCol + 1] == 'O')
+                    {
+                        if (matrix[heroRow, heroCol + 1] == matrix[firstPillarRow, firstPillarCol])
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow, heroCol + 1] = '-';
+                            matrix[secondPillarRow, secondPillarColl] = 'S';
+                            heroRow = secondPillarRow;
+                            heroCol = secondPillarColl;
+                        }
+                        else
+                        {
+                            matrix[heroRow, heroCol] = '-';
+                            matrix[heroRow, heroCol + 1] = '-';
+                            matrix[firstPillarRow, firstPillarCol] = 'S';
+                            heroRow = firstPillarRow;
+                            heroCol = firstPillarCol;
+                        }
+                    }
+                    else if (char.IsDigit(matrix[heroRow, heroCol + 1]))
+                    {
+                        sum += int.Parse(matrix[heroRow, heroCol + 1].ToString());
+                        matrix[heroRow, heroCol + 1] = 'S';
+                        matrix[heroRow, heroCol] = '-';
+                        heroCol++;
+                    }
+                    else
+                    {
+                        matrix[heroRow, heroCol] = '-';
+                        matrix[heroRow, heroCol + 1] = 'S';
+                        heroCol++;
+                    }
+                }
+            }
+
+            if (isOutOfBounds)
+            {
+                Console.WriteLine("Bad news, you are out of the bakery.");
+            }
+            else
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+
+            Console.WriteLine($"Money: {sum}");
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
