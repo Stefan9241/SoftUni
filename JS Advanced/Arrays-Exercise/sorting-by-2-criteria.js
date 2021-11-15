97654833f4b7ebb283a8a7229ea2c58e8467053b
stefan spirov <ssspirov@abv.bg>
2021-11-15 18:38:40 +0200

    exercise

---
+++ JS Advanced/Arrays-Exercise/sorting-by-2-criteria.js
@@ -0,0 +1,27 @@
+function solve(array) {
+    array.sort((a, b) => {
+        if (a.length === b.length) {
+            return a.localeCompare(b);
+        }
+
+        return a.length - b.length;
+    })
+
+    console.log(array.join('\n'));
+}
+
+solve(['alpha',
+    'beta',
+    'gamma']);
+
+solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+);
+
+solve(['test', 
+'Deny', 
+'omen', 
+'Default']);
