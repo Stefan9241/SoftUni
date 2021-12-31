df61860638f4ac511b8b3d0b874083f99445e89e
stefan spirov <ssspirov@abv.bg>
2021-12-31 18:33:09 +0200

    exercise

---
+++ JS Advanced/Advanced-Functions-Exercise/01. Sort Array.js
@@ -0,0 +1,11 @@
+function solve(array,string){
+
+    if(string == 'asc'){
+        return array.sort((a,b) => a - b);
+    }
+    else{
+        return array.sort((a,b)=> b - a);
+    }
+}
+
+console.log(solve([14, 7, 17, 6, 8], 'asc'));
