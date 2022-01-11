7a3b4c3c483821c1edbf46bd94b650f86eb5160b
stefan spirov <ssspirov@abv.bg>
2022-01-11 19:11:23 +0200

    lab

---
+++ JS Advanced/Prototypes-Lab/01. Person.js
@@ -0,0 +1,36 @@
+function Person(firstName, lastName) {
+  let result = {
+    firstName,
+    lastName,
+  };
+
+  Object.defineProperty(
+    result,
+    "fullName",
+    {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(value) {
+        let [first, last] = value.split(" ");
+        if (first != undefined && last != undefined) {
+          this.firstName = first;
+          this.lastName = last;
+        }
+      },
+    },
+    (enumerable = true),
+    (configurable = true)
+  );
+  return result;
+}
+
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
