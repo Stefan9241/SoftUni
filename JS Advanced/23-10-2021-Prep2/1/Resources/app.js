c510eadbe6a0580dc5bf760a44802fa4faf3e8a0
stefan spirov <ssspirov@abv.bg>
2022-01-18 18:06:08 +0200

    new solution for 1 problem

--- JS Advanced/23-10-2021-Prep2/1/Resources/app.js
+++ JS Advanced/23-10-2021-Prep2/1/Resources/app.js
@@ -1,18 +1,18 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  let genreElement = document.querySelector('input[name="genre"]');
-  let nameElement = document.querySelector('input[name="name"]');
-  let authorElement = document.querySelector('input[name="author"]');
-  let dateElement = document.querySelector('input[name="date"]');
+  let genreElement = document.getElementById("genre");
+  let nameElement = document.getElementById("name");
+  let authorElement = document.getElementById("author");
+  let dateElement = document.getElementById("date");
   let buttonAddElement = document.getElementById("add-btn");
   let collectionOfSongsDiv = document.querySelector(
     "div[class=all-hits-container]"
   );
-  let savedElemet = document.querySelector('.saved-container');
+  let savedElemet = document.querySelector(".saved-container");
   let likesElement = document.querySelector("div .likes p");
   buttonAddElement.addEventListener("click", addSong);
-
+  let counter = 1;
   function addSong(e) {
     e.preventDefault();
 
@@ -21,52 +21,56 @@ function solve() {
     let author = authorElement.value;
     let date = dateElement.value;
 
-    if (genre == "" || namee == "" || author == "" || date == "") {
-      return;
+    if (genre != "" && namee != "" && author != "" && date != "") {
+      let songDiv = createEl(
+        "div",
+        { className: "hits-info" },
+        createEl("img", { src: "./static/img/img.png" }),
+        createEl("h2", {}, `Genre: ${genre}`),
+        createEl("h2", {}, `Name: ${namee}`),
+        createEl("h2", {}, `Author: ${author}`),
+        createEl("h3", {}, `Date: ${date}`)
+      );
+
+      let saveButton = createEl("button", { className: "save-btn" }, "Save song");
+      let likeButton = createEl("button", { className: "like-btn" }, "Like song");
+      let deleteButton = createEl("button", { className: "delete-btn" }, "Delete");
+
+      saveButton.addEventListener("click", save);
+      likeButton.addEventListener("click", like);
+      deleteButton.addEventListener("click", deletee);
+
+      songDiv.appendChild(saveButton);
+      songDiv.appendChild(likeButton);
+      songDiv.appendChild(deleteButton);
+
+      collectionOfSongsDiv.appendChild(songDiv);
     }
-
-    let songDiv = createEl(
-      "div",
-      { className: "hits-info" },
-      createEl("img", { src: "./static/img/img.png" }),
-      createEl("h2", {}, `Genre: ${genre}`),
-      createEl("h2", {}, `Name: ${namee}`),
-      createEl("h2", {}, `Author: ${author}`),
-      createEl("h3", {}, `Date: ${date}`)
-    );
-
-    let saveButton = createEl("button", { id: "save-btn" }, "Save song");
-    let likeButton = createEl("button", { id: "like-btn" }, "Like song");
-    let deleteButton = createEl("button", { id: "delete-btn" }, "Delete");
-
-    songDiv.appendChild(saveButton);
-    songDiv.appendChild(likeButton);
-    songDiv.appendChild(deleteButton);
-
-    songDiv.addEventListener("click", manager);
-    collectionOfSongsDiv.appendChild(songDiv);
 
     genreElement.value = "";
     nameElement.value = "";
     authorElement.value = "";
     dateElement.value = "";
-
-    function manager(e) {
-      if (e.target.tagName == "BUTTON") {
-        if (e.target.textContent == "Save song") {
-          let divToSave = e.target.parentElement;
-          divToSave.removeChild(likeButton);
-          divToSave.removeChild(saveButton);
-          savedElemet.appendChild(divToSave);
-        } else if (e.target.textContent == "Like song") {
-          e.target.disabled = true;
-        } else if (e.target.textContent == "Delete") {
-          e.target.parentElement.remove();
-        }
-      }
-    }
   }
 
+  function like(e) {
+    let text = `Total Likes: ${counter}`;
+    e.target.disabled = true;
+    likesElement.textContent = text;
+    counter++;
+  }
+  function deletee(e) {
+    e.target.parentElement.remove();
+  }
+  function save(e) {
+    let divToSave = e.target.parentElement;
+    let likeButton = e.target.parentElement.querySelector(".like-btn");
+    let saveButton = e.target.parentElement.querySelector(".save-btn");
+
+    divToSave.removeChild(likeButton);
+    divToSave.removeChild(saveButton);
+    savedElemet.appendChild(divToSave);
+  }
   function createEl(type, props, ...content) {
     const element = document.createElement(type);
 
