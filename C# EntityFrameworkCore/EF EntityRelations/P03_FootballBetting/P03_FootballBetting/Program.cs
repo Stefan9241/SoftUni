1240a612aeb6f6f831ab7ccde0af7e4ef4a17a6a
stefan spirov <ssspirov@abv.bg>
2022-06-09 14:40:15 +0300

    betting

---
+++ C# EntityFrameworkCore/EF EntityRelations/P03_FootballBetting/P03_FootballBetting/Program.cs
@@ -0,0 +1,15 @@
+﻿using P03_FootballBetting.Data.Models;
+using System;
+
+namespace P03_FootballBetting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = new FootballBettingContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        }
+    }
+}
