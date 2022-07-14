3f4e31907482c29063050ff2eab084688373d3b5
stefan spirov <ssspirov@abv.bg>
2022-07-14 19:34:30 +0300

    8Aug2020

---
+++ C# EntityFrameworkCore/Exams/08-AUG-2020/Exam/VaporStore/Data/Models/Purchase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Purchase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public PurchaseType Type { get; set; }
+
+        [Required]
+        public string ProductKey { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ForeignKey("Card")]
+        public int CardId { get; set; }
+        public Card Card { get; set; }
+
+        [ForeignKey("Game")]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+
+    }
+}
